5cde15fdb42a38bef51075c1c09014f32df45a8f
何雨航 <453542772@qq.com>
2016-11-05 19:11:20 +0800

    123

--- #SETUP#.bat
+++ #SETUP#.bat
@@ -1,13 +1,14 @@
+setlocal enabledelayedexpansion
 cd ..
 
 if not exist "debug.cfg" (
 set /p DebugFileName=Debug文件名:
-echo %DebugFileName% > debug.cfg
+echo !DebugFileName! > debug.cfg
 )
 
 if not exist "main.cfg" (
 set /p MainFileName=程序文件名:
-echo %MainFileName% > main.cfg
+echo !MainFileName! > main.cfg
 )
 
 copy /y .\DevEnvSys\#DEBUG#.bat .\
